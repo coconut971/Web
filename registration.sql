79c9b8a99a7c4f59f7f021d3759522347ee09717
coconut971 <franck.ramassamy971@gmail.com>
2020-06-11 01:28:04 +0200

    modif mineur

--- registration.sql
+++ registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 08 juin 2020 à 15:35
+-- Généré le : jeu. 11 juin 2020 à 01:23
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.4
 
@@ -24,6 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `annonce`
+--
+
+CREATE TABLE `annonce` (
+  `id_annonce` int(3) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `descriptions` varchar(255) NOT NULL,
+  `prix` varchar(10) NOT NULL,
+  `url_img1` varchar(500) DEFAULT NULL,
+  `url_img2` varchar(500) DEFAULT NULL,
+  `url_img3` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire`
+--
+
+CREATE TABLE `commentaire` (
+  `id` int(5) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `contenu` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `users`
 --
 
@@ -35,9 +63,7 @@ CREATE TABLE `users` (
   `tel` varchar(100) NOT NULL,
   `adresse` varchar(255) NOT NULL,
   `avatar` varchar(255) DEFAULT 'default.png',
-  `inscription` datetime DEFAULT CURRENT_TIMESTAMP
-  
-  
+  `inscription` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -46,11 +72,24 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `mdp`, `tel`, `adresse`, `avatar`, `inscription`) VALUES
 (1, 'Clément', 'clement.dufourlamartinie@ynov.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '0770015105', '38 rue de fontenay 94300 Vincennes', 'default.png', '2020-06-02 14:20:49'),
-(4, 'admin', 'admin@gmail.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '0613270658', '27 bis rue d estienne d orves 94160 Fontenay Sous bois', 'default.png', '2020-06-02 14:20:49');
+(4, 'admin', 'admin@gmail.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '0613270658', '27 bis rue d estienne d orves 94160 Fontenay Sous bois', 'default.png', '2020-06-02 14:20:49'),
+(5, 'coconut971', 'franck.ramassamy971@gmail.com', 'cb1ad2119d8fafb69566510ee712661f9f14b83385006ef92aec47f523a38358', '0761551556', 'chez naruto', 'default.png', '2020-06-10 14:52:17');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `annonce`
+--
+ALTER TABLE `annonce`
+  ADD PRIMARY KEY (`id_annonce`);
+
+--
+-- Index pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `users`
@@ -63,10 +102,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pour la table `annonce`
+--
+ALTER TABLE `annonce`
+  MODIFY `id_annonce` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
